b24d9cafdeae1850d6503d52f904fa6ae83aacdc
Francois Gervais <francoisgervais@gmail.com>
2023-01-09 08:18:21 -0500

    Remove the ism330dhcx support

--- app/src/main.c
+++ app/src/main.c
@@ -20,86 +20,12 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 
-static int ism330dhcx_acc_trig_cnt;
-static int ism330dhcx_gyr_trig_cnt;
-
-static void ism330dhcx_acc_trigger_handler(const struct device *dev,
-					   const struct sensor_trigger *trig)
-{
-	sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
-	ism330dhcx_acc_trig_cnt++;
-}
-
-static void ism330dhcx_gyr_trigger_handler(const struct device *dev,
-					   const struct sensor_trigger *trig)
-{
-	sensor_sample_fetch_chan(dev, SENSOR_CHAN_GYRO_XYZ);
-	ism330dhcx_gyr_trig_cnt++;
-}
-
-static void ism330dhcx_config(const struct device *ism330dhcx)
-{
-	struct sensor_value odr_attr, fs_attr;
-
-	/* set ISM330DHCX sampling frequency to 416 Hz */
-	odr_attr.val1 = 416;
-	odr_attr.val2 = 0;
-
-	if (sensor_attr_set(ism330dhcx, SENSOR_CHAN_ACCEL_XYZ,
-			    SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr) < 0) {
-		LOG_ERR("Cannot set sampling frequency for ISM330DHCX accel");
-		return;
-	}
-
-	sensor_g_to_ms2(16, &fs_attr);
-
-	if (sensor_attr_set(ism330dhcx, SENSOR_CHAN_ACCEL_XYZ,
-			    SENSOR_ATTR_FULL_SCALE, &fs_attr) < 0) {
-		LOG_ERR("Cannot set sampling frequency for ISM330DHCX accel");
-		return;
-	}
-
-	/* set ISM330DHCX gyro sampling frequency to 208 Hz */
-	odr_attr.val1 = 208;
-	odr_attr.val2 = 0;
-
-	if (sensor_attr_set(ism330dhcx, SENSOR_CHAN_GYRO_XYZ,
-			    SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr) < 0) {
-		LOG_ERR("Cannot set sampling frequency for ISM330DHCX gyro");
-		return;
-	}
-
-	sensor_degrees_to_rad(250, &fs_attr);
-
-	if (sensor_attr_set(ism330dhcx, SENSOR_CHAN_GYRO_XYZ,
-			    SENSOR_ATTR_FULL_SCALE, &fs_attr) < 0) {
-		LOG_ERR("Cannot set fs for ISM330DHCX gyro");
-		return;
-	}
-
-
-	struct sensor_trigger trig;
-
-	trig.type = SENSOR_TRIG_DATA_READY;
-	trig.chan = SENSOR_CHAN_ACCEL_XYZ;
-	sensor_trigger_set(ism330dhcx, &trig, ism330dhcx_acc_trigger_handler);
-
-	trig.type = SENSOR_TRIG_DATA_READY;
-	trig.chan = SENSOR_CHAN_GYRO_XYZ;
-	sensor_trigger_set(ism330dhcx, &trig, ism330dhcx_gyr_trigger_handler);
-}
-
-
 void main(void)
 {
 	int ret;
 	
 	const struct device *const cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-	const struct device *const ism330dhcx = DEVICE_DT_GET_ONE(st_ism330dhcx);
 
-	struct sensor_value accel1[3];
-	struct sensor_value gyro[3];
-	int cnt = 0;
 
 	if (app_event_manager_init()) {
 		LOG_ERR("Event manager not initialized");
@@ -114,37 +40,6 @@ void main(void)
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return;
-	}
-
-	if (!device_is_ready(ism330dhcx)) {
-		LOG_ERR("%s: device not ready.", ism330dhcx->name);
-		return;
-	}
-
-	ism330dhcx_config(ism330dhcx);
-
-	while (1) {
-		sensor_channel_get(ism330dhcx, SENSOR_CHAN_ACCEL_XYZ, accel1);
-		sensor_channel_get(ism330dhcx, SENSOR_CHAN_GYRO_XYZ, gyro);
-
-		/* Erase previous */
-		// LOG_INF("\0033\014");
-
-		LOG_INF("ISM330DHCX: Accel (m.s-2): x: %.3f, y: %.3f, z: %.3f",
-			sensor_value_to_double(&accel1[0]),
-			sensor_value_to_double(&accel1[1]),
-			sensor_value_to_double(&accel1[2]));
-
-		LOG_INF("ISM330DHCX: GYro (dps): x: %.3f, y: %.3f, z: %.3f",
-			sensor_value_to_double(&gyro[0]),
-			sensor_value_to_double(&gyro[1]),
-			sensor_value_to_double(&gyro[2]));
-
-		LOG_INF("%d:: ism330dhcx acc trig %d", cnt, ism330dhcx_acc_trig_cnt);
-		LOG_INF("%d:: ism330dhcx gyr trig %d", cnt, ism330dhcx_gyr_trig_cnt);
-
-		cnt++;
-		k_sleep(K_MSEC(2000));
 	}
 
 	LOG_INF("****************************************");
